6bb00a5c836e3f1055ff2140b09e342c8e0a0b86
mypawreport <mypawreport@gmail.com>
2026-06-01 03:21:21 -0400

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE = 'paw-v2';
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(clients.claim()); });
+self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(fetch(e.request).catch(() => caches.match('/')));
+  }
+});
