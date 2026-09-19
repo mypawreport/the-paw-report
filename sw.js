b112e2fce5d71bb766303be4fb3031d4585fb643
mypawreport <mypawreport@gmail.com>
2026-09-19 04:36:30 -0500

    sw cache bump v343

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'paw-report-v342';
+const CACHE_NAME = 'paw-report-v343';
 const ASSETS = [
   'https://mypawreport.github.io/the-paw-report/',
   'https://mypawreport.github.io/the-paw-report/index.html',
